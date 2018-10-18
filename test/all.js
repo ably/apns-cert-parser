114d05f59c348ed53644116de4a3f7b13c225b9f
Paddy Byers <paddy.byers@gmail.com>
2018-10-18 16:01:50 +0100

    Support PEM text with escaped whitespace and preamble text; use the UID part of the subject distinguished name if the universal extension isn't present

--- test/all.js
+++ test/all.js
@@ -1,20 +1,62 @@
-const { Certificate } = require('../lib/certificate');
+const { Certificate, APPLE_UNIVERSAL_CERTIFICATE_EXTENSION } = require('../lib/certificate');
 
 const fs = require('fs');
 
+/* read a certificate with the universal extension, DER format */
 exports.read_der = function(test) {
-	const buf = fs.readFileSync(__dirname + '/fixtures/io.ably.push-demo.production-cert.der'),
+	const buf = fs.readFileSync(__dirname + '/fixtures/universal.der'),
 		cert = Certificate.fromDER(buf),
-		topicIdentifier = cert.getFirstTopicIdentifier();
+		topicIdentifier = cert.getTopicIdentifier();
 
 	test.equal(topicIdentifier.name, 'io.ably.push-demo', 'Verify expected topic id');
 	test.done();
 };
 
+/* read a certificate with the universal extension, PEM format */
 exports.read_pem = function(test) {
-	const buf = fs.readFileSync(__dirname + '/fixtures/io.ably.push-demo.production-cert.pem'),
+	const buf = fs.readFileSync(__dirname + '/fixtures/universal.pem'),
 		cert = Certificate.fromPEM(buf),
-		topicIdentifier = cert.getFirstTopicIdentifier();
+		topicIdentifier = cert.getTopicIdentifier();
+
+	test.equal(topicIdentifier.name, 'io.ably.push-demo', 'Verify expected topic id');
+	test.done();
+};
+
+/* read a certificate with the universal extension, PEM format, with preamble text */
+exports.read_preamble_pem = function(test) {
+	const buf = fs.readFileSync(__dirname + '/fixtures/preamble.pem'),
+		cert = Certificate.fromPEM(buf),
+		topicIdentifier = cert.getTopicIdentifier();
+
+	test.equal(topicIdentifier.name, 'io.ably.push-demo', 'Verify expected topic id');
+	test.done();
+};
+
+/* read a certificate with the universal extension, PEM format with escaped whitespace */
+exports.read_pem_escaped = function(test) {
+	const buf = fs.readFileSync(__dirname + '/fixtures/universal-escaped.pem'),
+		cert = Certificate.fromPEM(buf),
+		topicIdentifier = cert.getTopicIdentifier();
+
+	test.equal(topicIdentifier.name, 'io.ably.push-demo', 'Verify expected topic id');
+	test.done();
+};
+
+/* read a certificate with the universal extension, PEM format, with preamble text */
+exports.read_preamble_escaped_pem = function(test) {
+	const buf = fs.readFileSync(__dirname + '/fixtures/preamble-escaped.pem'),
+		cert = Certificate.fromPEM(buf),
+		topicIdentifier = cert.getTopicIdentifier();
+
+	test.equal(topicIdentifier.name, 'io.ably.push-demo', 'Verify expected topic id');
+	test.done();
+};
+
+/* read a certificate with the development extension and UID, PEM format */
+exports.read_development_pem = function(test) {
+	const buf = fs.readFileSync(__dirname + '/fixtures/development.pem'),
+		cert = Certificate.fromPEM(buf, {ignoreExtensionOID: APPLE_UNIVERSAL_CERTIFICATE_EXTENSION}),
+		topicIdentifier = cert.getTopicIdentifier();
 
 	test.equal(topicIdentifier.name, 'io.ably.push-demo', 'Verify expected topic id');
 	test.done();
